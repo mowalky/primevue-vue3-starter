4256def0ea2ac97e374af8d711502fe55d5290ff
Mowalky <dev@mowalky.com>
2022-01-01 14:00:20 -0600

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<script setup></script>
+
+<template>
+  <div
+    class="
+      w-full
+      h-full
+      flex flex-column
+      align-items-center
+      justify-content-center
+    "
+  >
+    <router-view></router-view>
+  </div>
+</template>
+
+<style>
+body {
+  color: var(--text-color);
+  font-family: var(--font-family);
+}
+#reload-toast .p-toast-message .p-toast-message-content {
+  justify-content: center !important;
+}
+</style>
